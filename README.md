3386ab695e17d2f5298b69c4e182135804b5cbd9
henriquegardini <henrique.gardini@hotmail.com>
2024-08-14 18:47:28 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Microserviço responsável pela parte de carrinho de compras.
---
+++ src/main/java/techclallenge5/fiap/com/msCarrinhoCompra/MsCarrinhoCompraApplication.java
@@ -0,0 +1,13 @@
+package techclallenge5.fiap.com.msCarrinhoCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCarrinhoCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCarrinhoCompraApplication.class, args);
+	}
+
+}
